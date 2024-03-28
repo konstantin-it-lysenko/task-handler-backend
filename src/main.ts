27f6102b42043211d01ca480b66742e4c06c8ce6
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-28 18:10:40 +0200

    add: cors, dotenv config, api prefix, cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { PORT } = process.env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  app.setGlobalPrefix('api')
+  app.use(cookieParser())
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
+
+  await app.listen(PORT)
 }
 bootstrap()
