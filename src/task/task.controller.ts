ef49dcc3925fd2c2da0750bf0e8329f3a6262ffe
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-30 01:17:08 +0200

    add: get all tasks controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { TaskService } from './task.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
 
-@Controller('task/')
+@Controller('user/tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  @Auth()
+  async getAll(@CurrentUser('id') userId: string) {
+    return this.taskService.getAll(userId)
+  }
 }
