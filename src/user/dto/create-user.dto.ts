95194210e5098383d5ef8b1f4c30f3c328f4213b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-29 18:29:21 +0200

    add: user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator'
+
+export class UserDto {
+  @IsEmail()
+  @IsOptional()
+  email?: string
+
+  @IsString()
+  @IsOptional()
+  name?: string
+
+  @IsString()
+  @MinLength(6, {
+    message: 'Password must be at least 6 characters long'
+  })
+  @IsOptional()
+  password?: string
+}
