a625171fcd1af00a4e2194696a204f76feef29d6
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-30 01:10:21 +0200

    add: task create service

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
-// import { TaskDto } from './dto/task.dto'
+import { TaskDto } from './dto/task.dto'
 
 @Injectable()
 export class TaskService {
@@ -10,6 +10,19 @@ export class TaskService {
     return this.prisma.task.findMany({
       where: {
         userId
+      }
+    })
+  }
+
+  async create(dto: TaskDto, userId: string) {
+    return this.prisma.task.create({
+      data: {
+        ...dto,
+        user: {
+          connect: {
+            id: userId
+          }
+        }
       }
     })
   }
