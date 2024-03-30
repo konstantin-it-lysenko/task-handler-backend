226c5441d47b5a1491e5f14153bb1cc5d98396de
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-30 02:30:55 +0200

    add: create timeblock controller

--- src/time-block/time-block.controller.ts
+++ src/time-block/time-block.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
 import { TimeBlockService } from './time-block.service'
 import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { TimeBlockDto } from './dto/time-block.dto'
 
 @Controller('user/time-blocks')
 export class TimeBlockController {
@@ -11,5 +20,13 @@ export class TimeBlockController {
   @Auth()
   async getAll(@CurrentUser('id') userId: string) {
     return this.timeBlockService.getAll(userId)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  async create(@Body() dto: TimeBlockDto, @CurrentUser('id') userId: string) {
+    return this.timeBlockService.create(dto, userId)
   }
 }
