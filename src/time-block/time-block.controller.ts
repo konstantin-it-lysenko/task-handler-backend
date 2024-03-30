fc9c0d6cf6abd0e23f901527293260950e829216
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-30 02:31:38 +0200

    add: update timeblock order controller

--- src/time-block/time-block.controller.ts
+++ src/time-block/time-block.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   HttpCode,
   Post,
+  Put,
   UsePipes,
   ValidationPipe
 } from '@nestjs/common'
@@ -11,6 +12,7 @@ import { TimeBlockService } from './time-block.service'
 import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
 import { TimeBlockDto } from './dto/time-block.dto'
+import { UpdateOrderDto } from './dto/update-order-dto'
 
 @Controller('user/time-blocks')
 export class TimeBlockController {
@@ -28,5 +30,13 @@ export class TimeBlockController {
   @Auth()
   async create(@Body() dto: TimeBlockDto, @CurrentUser('id') userId: string) {
     return this.timeBlockService.create(dto, userId)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put('update-order')
+  @Auth()
+  updateOrder(@Body() updateOrderDto: UpdateOrderDto) {
+    return this.timeBlockService.updateOrder(updateOrderDto.ids)
   }
 }
